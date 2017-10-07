0b4ce151daed6765826f8c73e2288c1bef17d2fe
Sergey <Serge-shilovski@yandex.ru>
2017-10-08 02:00:07 +0300

    added another lang file

---
+++ package.nw/js/blockly/msg/bloks/lang/ru.js
@@ -0,0 +1,39 @@
+//ide_binar_logic.js
+
+Dron.Msg.LOGIG = "битовое";
+Dron.Msg.LOGIC_OPERATION_AND = 'битовая операция \"И\"';
+Dron.Msg.LOGIC_OPERATION_OR = 'битовая операция \"ИЛИ\"';
+Dron.Msg.LOGIC_BITWISE_SHIFT_LEFT = 'битовый сдвиг влево';
+Dron.Msg.LOGIC_BITWISE_SHIFT_RIGHT = 'битовый сдвиг вправо';
+
+//ide_break_block.js
+
+Dron.Msg.STOP_EXEC = 'Прервать выполнение';
+
+//ide_continue_block.js
+
+Dron.Msg.CONTINUE_EXEC = 'Продолжить выполнение';
+
+//ide_variables_declare_block.js
+
+Dron.Msg.DECLARE = 'Объявить';
+Dron.Msg.VARIABLE = 'переменную';
+Dron.Msg.CONSTANT = 'константу';
+Dron.Msg.INTEGER = 'целочисленного';
+Dron.Msg.UNSIGNED_INTEGER = 'целочисленного беззнакового';
+Dron.Msg.FLOAT = 'дробного';
+Dron.Msg.DOUBLE = 'дробного точного';
+Dron.Msg.TYPE = 'типа';
+
+//ide_variables_set_block.js
+
+Dron.Msg.SET_VARIABLE = 'Присвоить переменной';
+Dron.Msg.VALUE = 'значение';
+
+//state_block.js
+
+Dron.Msg.VALUE2 = 'Значение'; //????
+Dron.Msg.NOT_COMPARE = 'не совпадает';
+Dron.Msg.COMPARE_WITH = 'совпадает с';
+Dron.Msg.SELECTION_UNIT = 'Блок выбора';
+Dron.Msg.HEAD_SELECTION_UNIT = 'Головной блок выбора';
